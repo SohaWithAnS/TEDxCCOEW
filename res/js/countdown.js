9af2e24bb8c95e7a832279777d46f88e08072050
SohaWithAnS <soha141198@gmail.com>
2021-04-17 23:34:16 +0530

    info

--- res/js/countdown.js
+++ res/js/countdown.js
@@ -1,5 +1,5 @@
 $('.countdown-timer').countdown({
-    date: "April 18, 2021 10:00:00",
+    date: "April 18, 2021 10:30:00",
 
     render: function(data) {
         $(".days .value").html(this.leadingZeros(data.days, 2));
